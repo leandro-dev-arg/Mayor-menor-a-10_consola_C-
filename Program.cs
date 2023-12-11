aaf69040f177a698a64875001cb7e553ffcb56de
leandro <leandro.dev.arg@gmail.com>
2023-12-11 16:38:08 -0300

    primera subida

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace U3_programa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   int n1;
+            Console.WriteLine("ingrese un numero por favor");
+            n1=int.Parse(Console.ReadLine());
+            if (n1>10){
+                Console.WriteLine("Es mayor a 10");
+            }else{
+                Console.WriteLine("No es mayor a 10");
+            }
+        }
+    }
+}
